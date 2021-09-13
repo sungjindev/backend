fbbb6eacf5f2997554ec486483dd7768e2f51326
L-o-g-a-n <xy.logandev@gmail.com>
2021-09-13 20:27:27 +0900

    [ADD] (#1) - add some structures

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./api/routes');
-const { PORT, NODE_ENV } = require('./env');
+const { PORT } = require('./env');
 
 const app = express();
 
 app.set('port', PORT || 3003);
+
+app.use(cors({
+    credentials: true
+}));
 
 app.use('/', router);
 
