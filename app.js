7ac4d60135625d8a87ee75758059bb1d6ca0de38
L-o-g-a-n <xy.logandev@gmail.com>
2021-09-18 20:09:48 +0900

    [ADD] (#1) - add a mysql database

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./api/routes');
 const { PORT } = require('./env');
+const { sequelize } = require('./models');
 
 const app = express();
 
 app.set('port', PORT || 3003);
 
+sequelize.sync({ force: true })
+    .then(() => {
+        console.log('DB connected!');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 app.use(cors({
     credentials: true
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/', router);
 
