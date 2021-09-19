438889854d4ec409b98f832dc6c971a395ee0bbc
L-o-g-a-n <xy.logandev@gmail.com>
2021-09-19 17:51:18 +0900

    [ADD] (#5) - add a trainer model

---
+++ models/schemas/trainer.js
@@ -0,0 +1,39 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Trainer extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        trainerId: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          primaryKey: true,
+        },
+        trainerPassword: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        trainerPhoneNumber: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        trainerName: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Trainer",
+        tableName: "trainers",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {}
+};
