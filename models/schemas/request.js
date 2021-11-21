85270785b2ad0d1c056a00dfe835538978de5271
L-o-g-a-n <xy.logandev@gmail.com>
2021-11-21 20:43:57 +0900

    [ADD] (#21) - add a request model

---
+++ models/schemas/request.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Request extends Sequelize.Model { 
+  static init(sequelize) {
+    return super.init (
+      {
+        requestor: {
+          type: Sequelize.STRING(20)
+        },
+        requestee: {
+          type: Sequelize.STRING(20)
+        }
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "Request",
+        tableName: "requests",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+  }
+};
