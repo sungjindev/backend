a788793aa72d65cc96305968983e83cbf3d6fa35
L-o-g-a-n <xy.logandev@gmail.com>
2021-12-05 16:24:17 +0900

    [ADD] (#25) - add a putExpired router

--- api/routes/v1/member/index.js
+++ api/routes/v1/member/index.js
@@ -4,5 +4,6 @@ const controller = require('./controller');
 
 router.get('/', controller.getMembers);
 router.delete('/', controller.deleteMember);
+router.put('/expired', controller.putExpired);
 
 module.exports = router;
