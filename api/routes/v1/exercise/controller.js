3b516a4130e2b875f682ce253dd1ed0ae5366f4c
L-o-g-a-n <xy.logandev@gmail.com>
2022-01-10 22:38:20 +0900

    [FIX] (#31) - fix a getExercises router

--- api/routes/v1/exercise/controller.js
+++ api/routes/v1/exercise/controller.js
@@ -6,10 +6,16 @@ const { verifyToken } = require('../../../../utils/jwt');
 const getExercises = async(req,res,next) => {
   try {
     const accessToken = verifyToken(req.headers.authorization.split('Bearer ')[1]);
+    const part = ["가슴", "등", "어깨", "하체", "이두", "삼두", "복근", "유산소"];
     if(!accessToken)
       return next(JSON_WEB_TOKEN_ERROR);
   
-    const exercises = await Exercise.findAll();
+    const exercises = await Promise.all(
+      part.map(function(val) {
+      const temp = Exercise.findAll({where: {part: val}, raw: true});
+      return temp;
+      })
+    );
     return res.json(createResponse(res, exercises));
   } catch (error) {
     console.error(error);
