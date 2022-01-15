98a84fd91f5af76e1015a392225d08883ac7f5a5
L-o-g-a-n <xy.logandev@gmail.com>
2022-01-15 22:09:19 +0900

    [FIX] (#33) - fix a trainer schema

--- models/schemas/trainer.js
+++ models/schemas/trainer.js
@@ -16,6 +16,10 @@ module.exports = class Trainer extends Sequelize.Model {
           type: Sequelize.STRING(20),
           allowNull: false,
         },
+        goal: {
+          type: Sequelize.STRING(100),
+          defaultValue: null
+        }
       },
       {
         sequelize,
