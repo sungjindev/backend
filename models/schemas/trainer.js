fe940a3d14530464f157dd1f8e0618871724e121
L-o-g-a-n <xy.logandev@gmail.com>
2022-02-21 13:41:39 +0900

    [ADD] (#37) - add an introduction in a trainer schema

--- models/schemas/trainer.js
+++ models/schemas/trainer.js
@@ -16,7 +16,7 @@ module.exports = class Trainer extends Sequelize.Model {
           type: Sequelize.STRING(20),
           allowNull: false,
         },
-        goal: {
+        introduction: {
           type: Sequelize.STRING(100),
           defaultValue: null
         },
